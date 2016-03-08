f67b8b04227384f2b22c4e594297cce4519cb2de
Roger Ignazio <me@rogerignazio.com>
2016-03-08 13:01:58 -0800

    ability to customize docker storage driver
    This commit allows a user to customize the Docker storage driver
    by using the command line flag `--storage-driver=foo`, thus
    setting the storage driver to use with the Docker daemon running
    inside the parent Docker container.
    
    If this command line argument is not passed in as the first option,
    then the script will default to `overlay` per gh-56.

--- dind-agent/wrapper.sh
+++ dind-agent/wrapper.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 set -e
 
-echo "==> Launching the Docker daemon..."
-dind docker daemon --host=unix:///var/run/docker.sock --storage-driver=overlay &
+# If the first argument to this script is "--storage-driver=foo", then set
+# the variable $DOCKER_STORAGE_DRIVER to the passed in argument. Otherwise,
+# default to the "overlay" storage driver.
+if [[ "$1" =~ "--storage-driver" ]]; then
+    DOCKER_STORAGE_DRIVER="${1#*=}"
+    shift
+else
+    DOCKER_STORAGE_DRIVER="overlay"
+fi
+
+echo "==> Launching the Docker daemon with storage driver '${DOCKER_STORAGE_DRIVER}' ..."
+dind docker daemon --host=unix:///var/run/docker.sock --storage-driver=${DOCKER_STORAGE_DRIVER} &
 
 while(! docker info > /dev/null 2>&1); do
     echo "==> Waiting for the Docker daemon to come online..."
